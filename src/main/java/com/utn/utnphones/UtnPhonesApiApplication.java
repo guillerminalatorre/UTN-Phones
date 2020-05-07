4b648650ec1e197cbd7cbd04a79ef391030d2277
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2020-05-07 12:07:39 -0300

    spring boot page

---
+++ src/main/java/com/utn/utnphones/UtnPhonesApiApplication.java
@@ -0,0 +1,13 @@
+package com.utn.utnphones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnPhonesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnPhonesApiApplication.class, args);
+	}
+
+}
